af9e404f3721c748d508f8d483b33ec35116219d
Hugo Alves <ha8917@gmail.com>
2021-06-30 08:59:12 -0400

    update

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var input = document.getElementById("user-input");
 var ul = document.querySelector("ul");
 
 button.addEventListener("click", function() {
-    if (input, length > 0) {
+    if (input.value > 0) {
     }
     var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input));
+    li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
 })
